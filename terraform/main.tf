6e16aae825aefb7509de6d005a2179909a816b85
Jonathan Steffan <jsteffan@fedoraproject.org>
2021-11-08 20:31:52 -0700

    Disable terraform k8s for now.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,36 +21,36 @@ terraform {
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#full-reference
 provider "google" {}
 
-# Configure kubernetes provider with Oauth2 access token.
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
-# This fetches a new token, which will expire in 1 hour.
-data "google_client_config" "fspin" {
-  depends_on = [module.k8s]
-}
-
-# Defer reading the cluster data until the GKE cluster exists.
-data "google_container_cluster" "fspin" {
-  name = local.cluster_name
-  depends_on = [module.k8s]
-}
-
-provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.fspin.endpoint}"
-  token = data.google_client_config.fspin.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.fspin.master_auth[0].cluster_ca_certificate,
-  )
-}
-
-provider "helm" {
-  kubernetes {
-    host  = "https://${data.google_container_cluster.fspin.endpoint}"
-    token = data.google_client_config.fspin.access_token
-    cluster_ca_certificate = base64decode(
-      data.google_container_cluster.fspin.master_auth[0].cluster_ca_certificate,
-    )
-  }
-}
+## Configure kubernetes provider with Oauth2 access token.
+## https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
+## This fetches a new token, which will expire in 1 hour.
+#data "google_client_config" "fspin" {
+#  depends_on = [module.k8s]
+#}
+#
+## Defer reading the cluster data until the GKE cluster exists.
+#data "google_container_cluster" "fspin" {
+#  name = local.cluster_name
+#  depends_on = [module.k8s]
+#}
+#
+#provider "kubernetes" {
+#  host  = "https://${data.google_container_cluster.fspin.endpoint}"
+#  token = data.google_client_config.fspin.access_token
+#  cluster_ca_certificate = base64decode(
+#    data.google_container_cluster.fspin.master_auth[0].cluster_ca_certificate,
+#  )
+#}
+#
+#provider "helm" {
+#  kubernetes {
+#    host  = "https://${data.google_container_cluster.fspin.endpoint}"
+#    token = data.google_client_config.fspin.access_token
+#    cluster_ca_certificate = base64decode(
+#      data.google_container_cluster.fspin.master_auth[0].cluster_ca_certificate,
+#    )
+#  }
+#}
 
 module "gcp" {
   source         = "./gcp"
