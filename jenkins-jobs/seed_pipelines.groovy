821a66c932b18675303c302629acb9a76c0df5f8
Jonathan Steffan <jsteffan@fedoraproject.org>
2024-03-06 19:16:21 -0700

    Add Jenkins seed pipelines groovy.

---
+++ jenkins-jobs/seed_pipelines.groovy
@@ -0,0 +1,46 @@
+pipelineJob("snapshot-pipeline") {
+	description("""Pipeline to create a new repository snapshot.""")
+	displayName("Create Snapshot")
+	parameters {
+		choiceParam("Release Version", [39, 40], "Fedora release version.")
+		stringParam("Snapshot Count", "5", "Number of snapshots to keep on mirror storage.")
+		stringParam("Mirror Host", "ftp-chi.osuosl.org", "Mirror to sync from for initial pass. Always checks upstream main mirror after first pass.")
+		stringParam("Mirror Path", "fedora/linux", "Base Fedora path on the mirror.")
+		stringParam("Mirror Exclude", "debug/*", "Pathspec in the mirror to not pull down.")
+	}
+	definition {
+		cpsScm {
+			scm {
+		        	git {
+		        		remote {
+		       				url 'https://github.com/fspin-k8s/fspin-infrastructure.git'
+					}
+					branch('main')
+				}
+			}
+			scriptPath('jenkins-jobs/snapshot-pipeline')
+		}
+	}
+}
+pipelineJob("builder-pipeline") {
+	description("Update the GCE build machine image to the latest snapshot.")
+	displayName("Update Build VM Image")
+	parameters {
+		choiceParam("Release Version", [39, 40], "Fedora release version.")
+		booleanParam("Enable Updates Testing", false, "Enable the updates-testing repository for the builder update.  Does not affect repository contents.")
+		stringParam("Image Count", "5", "Number of machine images to keep in GCE.")
+	}
+	definition {
+		cpsScm {
+			scm {
+		        	git {
+		        		remote {
+		       				url 'https://github.com/fspin-k8s/fspin-infrastructure.git'
+					}
+					branch('main')
+				}
+			}
+			scriptPath('jenkins-jobs/buildvm-pipeline')
+		}
+	}
+}
